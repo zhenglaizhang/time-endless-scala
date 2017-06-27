f02b588fe58dd1740582766832da29127ea079b7
Zhenglai Zhang <zhenglai.zhang@quadas.com>
2017-06-27 16:42:41 +0800

    enable scheduling

--- src/main/scala/com/lianji/te/ApplicationConfig.scala
+++ src/main/scala/com/lianji/te/ApplicationConfig.scala
@@ -2,11 +2,13 @@ package com.lianji.te
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableScheduling
 
 //@Configuration // the annotated class contains Spring configuration definitions such as the @Bean, @Component, and @Service declarations
 //@EnableAutoConfiguration
 //@ComponentScan // scan our application packages—starting from the package of our annotated class as a default package root
 @SpringBootApplication
+@EnableScheduling
 class ApplicationConfig {
   @Bean
   def schedulerRunner = new StartupRunner
--- src/main/scala/com/lianji/te/StartupRunner.scala
+++ src/main/scala/com/lianji/te/StartupRunner.scala
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.core.annotation.Order
+import org.springframework.scheduling.annotation.Scheduled
 
 /*
 Command line runners are a useful functionality to execute the various types of code that
@@ -25,7 +26,8 @@ class StartupRunner extends CommandLineRunner {
  Spring Boot recognized that we’ve autowired a DataSource dependency and automatically created one initializing the in-memory H2 data store.
 
 @Bean tells Spring 'here is an instance of this class, please keep hold of it and give it back to me when I ask'.
-Annotating@Bean only register this service as a bean(i.e a kind of Object) in spring application context. In simple word, it is just registration but nothing.
+Annotating@Bean only register this service as a bean(i.e a kind of Object) in spring application context. In simple word, it is just
+registration but nothing.
 
 @Autowired says 'please give me an instance of this class, for example, one that I created with an @Bean annotation earlier'.
 Annotating a variable with @Autowired inject a BookingService bean(i.e Object) from Spring Application Context.
@@ -44,6 +46,24 @@ Annotating a variable with @Autowired inject a BookingService bean(i.e Object) f
     log.info("Datasource: {}", ds.toString)
     log.info("book counts: {}", bookRepository.count())
     log.info("photo counts: {}", photoRepository.count())
+  }
+
+  /*
+  @EnableScheduling is not a Spring Boot annotation, but instead is a Spring
+Context module annotation. Similar to the @SpringBootApplication and
+@EnableAutoConfiguration annotations, this is a meta-annotation and internally imports
+the SchedulingConfiguration via the @Import(SchedulingConfiguration.class)
+instruction, which can be seen if looked found inside the code for the @EnableScheduling
+annotation class.
+ScheduledAnnotationBeanPostProcessor that will be created by the imported
+configuration will scan the declared Spring Beans for the presence of the @Scheduled
+annotations. For every annotated method without arguments, the appropriate executor
+thread pool will be created. It will manage the scheduled invocation of the annotated
+method
+   */
+  @Scheduled(initialDelay = 1000, fixedRate = 10000)
+  def run(): Unit = {
+    log.info("Num of books={}", bookRepository.count())
   }
 }
 
