6f97bc219e9a926e60c78b1022559f5d70e12fc2
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-06-27 22:25:31 +0800

    message converters

--- src/main/scala/com/lianji/te/WebConfiguration.scala
+++ src/main/scala/com/lianji/te/WebConfiguration.scala
@@ -4,7 +4,7 @@ import javax.servlet.Filter
 
 import org.apache.catalina.filters.RemoteIpFilter
 import org.springframework.context.annotation.{ Bean, Configuration }
-import org.springframework.web.servlet.HandlerInterceptor
+import org.springframework.http.converter.ByteArrayHttpMessageConverter
 import org.springframework.web.servlet.config.annotation.{ InterceptorRegistry, WebMvcConfigurerAdapter }
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
 
@@ -37,4 +37,34 @@ WebMvcConfigurer class if we want to have some logical separation.
     super.addInterceptors(registry)
     registry.addInterceptor(localeChangeInterceptor)
   }
+
+
+  /*
+   Spring Boot automatically configured HttpMessageConverters to translate our
+entity beans objects into a JSON representation using Jackson library, writing the resulting
+JSON data to an HTTP response output stream. When multiple converters are available,
+the most applicable one gets selected based on the message object class and the requested
+content type.
+
+If Spring detects a bean of the HttpMessageConverter type, it will add it
+to the list automatically.
+   */
+  @Bean
+  def byteArrayHttpMessageConverter: ByteArrayHttpMessageConverter = new ByteArrayHttpMessageConverter
+
+  // same as above
+  /*
+there is no guarantee that our method can get called in any particular order
+   */
+//  override def configureMessageConverters(converters: util.List[HttpMessageConverter[_]]) =
+//    converters.add(new ByteArrayHttpMessageConverter)
+
+  /*
+  . This method gets invoked after all the WebMvcConfigurers get called for configureMessageConverters and the list of converters is fully populated
+   */
+  // more control
+//  override def extendMessageConverters(converters: util.List[HttpMessageConverter[_]]) = {
+//    converters.clear()
+//    converters.add(new ByteArrayHttpMessageConverter)
+//  }
 }
