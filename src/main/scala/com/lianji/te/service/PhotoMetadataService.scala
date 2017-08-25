923b326a5d7b8a1a8b9349fc72bc43d75dca7db1
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2017-08-25 22:49:42 +0800

    .

--- src/main/scala/com/lianji/te/service/PhotoMetadataService.scala
+++ src/main/scala/com/lianji/te/service/PhotoMetadataService.scala
@@ -15,50 +15,64 @@ import org.springframework.stereotype.Service
 @Service
 class PhotoMetadataService {
   def getPhoto(category: java.util.List[Category], name: String, desc: String, url: String, inputStream: InputStream): Photo = {
-    val metadata = ImageMetadataReader.readMetadata(inputStream)
-    val exifDir = metadata.getFirstDirectoryOfType(classOf[ExifSubIFDDirectory])
-    val exifDesc = new ExifSubIFDDescriptor(exifDir)
+    try {
+      val metadata = ImageMetadataReader.readMetadata(inputStream)
+      val exifDir = metadata.getFirstDirectoryOfType(classOf[ExifSubIFDDirectory])
+      val exifDesc = new ExifSubIFDDescriptor(exifDir)
 
-    val exifD0Dir = metadata.getFirstDirectoryOfType(classOf[ExifIFD0Directory])
+      val exifD0Dir = metadata.getFirstDirectoryOfType(classOf[ExifIFD0Directory])
 
 
-    val width = exifD0Dir.getInt(ExifDirectoryBase.TAG_IMAGE_WIDTH)
-    val height = exifD0Dir.getInt(ExifDirectoryBase.TAG_IMAGE_HEIGHT)
-    val make = exifD0Dir.getString(ExifDirectoryBase.TAG_MAKE)
-    val model = exifD0Dir.getString(ExifDirectoryBase.TAG_MODEL)
-    val copyright = exifD0Dir.getString(ExifDirectoryBase.TAG_COPYRIGHT)
+      val width = exifD0Dir.getInt(ExifDirectoryBase.TAG_IMAGE_WIDTH)
+      val height = exifD0Dir.getInt(ExifDirectoryBase.TAG_IMAGE_HEIGHT)
+      val make = exifD0Dir.getString(ExifDirectoryBase.TAG_MAKE)
+      val model = exifD0Dir.getString(ExifDirectoryBase.TAG_MODEL)
+      val copyright = exifD0Dir.getString(ExifDirectoryBase.TAG_COPYRIGHT)
 
-    val fNumber = exifDesc.getFNumberDescription
-    val originalDate = exifDir.getDate(ExifDirectoryBase.TAG_DATETIME_ORIGINAL)
-    val exposureTime = exifDesc.getExposureTimeDescription
-    val exposureProgram = exifDesc.getExposureProgramDescription
-    val meteringMode = exifDesc.getMeteringModeDescription
-    val iso = exifDir.getInt(ExifDirectoryBase.TAG_ISO_EQUIVALENT)
-    val apertureValue = exifDesc.getApertureValueDescription
-    val maxApertureValue = exifDesc.getMaxApertureValueDescription
-    val focalLength = exifDesc.getFocalLengthDescription
-    Photo(id = null,
-           category = category.asScala.mkString(","),
-           name = name,
-           description = desc,
-           dateTimeOriginal = new java.sql.Date(originalDate.getTime).toLocalDate,
-           width = width,
-           height = height,
-           exposureTime = exposureTime,
-           meteringMode = meteringMode,
-           exposureProgram = exposureProgram,
-           fNumber = fNumber,
-           model = model,
-           make = make,
-           copyright = copyright,
-           isoSpeedRatings = iso,
-           apertureValue = apertureValue,
-           maxApertureValue = maxApertureValue,
-           focalLength = focalLength,
-           url_mobile = url,
-           url_mobile_index = url,
-           url = url,
-           url_index = url)
+      val fNumber = exifDesc.getFNumberDescription
+      val originalDate = exifDir.getDate(ExifDirectoryBase.TAG_DATETIME_ORIGINAL)
+      val exposureTime = exifDesc.getExposureTimeDescription
+      val exposureProgram = exifDesc.getExposureProgramDescription
+      val meteringMode = exifDesc.getMeteringModeDescription
+      val iso = exifDir.getInt(ExifDirectoryBase.TAG_ISO_EQUIVALENT)
+      val apertureValue = exifDesc.getApertureValueDescription
+      val maxApertureValue = exifDesc.getMaxApertureValueDescription
+      val focalLength = exifDesc.getFocalLengthDescription
+      Photo(id = null,
+        category = category.asScala.mkString(","),
+        name = name,
+        description = desc,
+        dateTimeOriginal = new java.sql.Date(originalDate.getTime).toLocalDate,
+        width = width,
+        height = height,
+        exposureTime = exposureTime,
+        meteringMode = meteringMode,
+        exposureProgram = exposureProgram,
+        fNumber = fNumber,
+        model = model,
+        make = make,
+        copyright = copyright,
+        isoSpeedRatings = iso,
+        apertureValue = apertureValue,
+        maxApertureValue = maxApertureValue,
+        focalLength = focalLength,
+        url_mobile = url,
+        url_mobile_index = url,
+        url = url,
+        url_index = url)
+    } catch {
+      case _: Throwable =>
+        Photo().copy(
+          id = null,
+          category = category.asScala.mkString(","),
+          name = name,
+          description = desc,
+          url_mobile = url,
+          url_mobile_index = url,
+          url = url,
+          url_index = url)
+    }
+
   }
 
   def crapImgInputStream(inputStream: InputStream, size: Int): ByteArrayInputStream = {
